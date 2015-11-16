66ad6c14f36a10a4151d913520d384455a883f8b
iamdanielkim <iamdanielkim@coupang.com>
2015-11-16 19:54:28 +0900

    #2 TDD Sample 만들기 - FizzBuzz

---
+++ exercises_KR/refresh_cache.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+require_relative '../admin_scripts/lib_domain'
+
+cache_filename = dojo.caches.path + Exercises.cache_filename
+`chmod 666 #{cache_filename}`
+dojo.exercises.refresh_cache
+`chmod 444 #{cache_filename}`
+`chown www-data:www-data #{cache_filename}`
